82f77ca68f821fe6765ddba2e08cb178a0e553f7
mubashir <imubasherahmad@gmail.com>
2021-08-12 02:37:50 +0500

    Add header text

--- main.js
+++ main.js
@@ -6,16 +6,18 @@ function processRawCode(codeSnippet){
 
     rawCode = codeSnippet.innerHTML;
     rawCode = rawCode.split("\n");
-
+    numberOfLines = rawCode.length;
+    
     codeSnippet.innerHTML = "";
 
     snippetHeader = document.createElement('div');
     snippetHeader.className = 'header';
     codeSnippet.appendChild(snippetHeader);
-    
+
     snippetBody = document.createElement('table');
     codeSnippet.appendChild(snippetBody);
 
+    snippetSize = 0;
     for(j = 0; j < rawCode.length; j++) {
         row = snippetBody.appendChild(document.createElement('tr'));
         lineNumber = row.appendChild(document.createElement('td'));
@@ -24,6 +26,23 @@ function processRawCode(codeSnippet){
 
         lineCode = row.appendChild(document.createElement('td'));
         lineCode.className = 'line-code';
-        lineCode.innerHTML = rawCode[j]; 
+        lineCode.innerHTML = rawCode[j];
+
+        snippetSize += rawCode[j].length;
     }
+    
+    snippetHeader.innerHTML = numberOfLines + " lines " + " | " + getSizeUnit(snippetSize);
+}
+
+function getSizeUnit(snippetSize){
+
+    units = ['Bytes', 'KB', 'MB'];
+    steps = 0;
+    while(snippetSize >= 1024){
+        snippetSize /= 1024;
+        snippetSize = Math.round(snippetSize * 100)/100
+        steps++;
+    }
+    
+    return snippetSize + " " + units[steps];
 }
