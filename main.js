bf9cb97a6a5c71c1fbde4b445a813fe2cc850beb
Haris Manzoor <45047495+haris741@users.noreply.github.com>
2021-11-04 17:22:27 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 let codeSnippets = document.getElementsByClassName('code-snippet');
 for (let i = 0; i < codeSnippets.length; i++)
-    processRawCode(codeSnippets[i]); 
+    processRawCode(codeSnippets[i]);
 
 function processRawCode(codeSnippet){
 
     let rawCode = codeSnippet.textContent;
     rawCode = rawCode.split("\n");
     let numberOfLines = rawCode.length;
-    
+
     codeSnippet.innerHTML = "";
 
     let snippetHeader = document.createElement('div');
     snippetHeader.className = 'header';
     codeSnippet.appendChild(snippetHeader);
-    
+
     let snippetBodyDiv = document.createElement('div');
     snippetBodyDiv.className = 'body';
     codeSnippet.appendChild(snippetBodyDiv);
-    
+
     let snippetBody = document.createElement('table');
     snippetBodyDiv.appendChild(snippetBody);
 
@@ -34,7 +34,7 @@ function processRawCode(codeSnippet){
 
         snippetSize += rawCode[i].length;
     }
-    
+
     snippetHeader.innerHTML = numberOfLines + " lines " + " | " + getSizeUnit(snippetSize);
 }
 
@@ -46,12 +46,12 @@ function isKeyword(line, stIndex, endIndex) {
             if (line[endIndex + 1] == ' ' || line[endIndex + 1] == ':') return true;
             else return false;
         }
-    } 
+    }
     else {
         if (line[stIndex - 1] != ' ') return false;
         else {
             if (endIndex + 1 == line.length) return true;
-            else 
+            else
             {
                 if (line[endIndex + 1] == ' ' || line[endIndex + 1] == ':') return true;
                 else return false;
@@ -64,20 +64,38 @@ function isKeyword(line, stIndex, endIndex) {
 function styleKeywords(line){
     let redKeywords = ['as', 'assert', 'async', 'await','break', 'class', 'continue', 'def', 'elif', 'except', 'for', 'global', 'if', 'import', 'lambda', 'nonlocal', 'return', 'with', 'yield'];
     let blueKeywords = ['False', 'in', 'is', 'not', 'None', 'True']
+	var words = line.split(' ');
 
     for(let i = 0; i < redKeywords.length; i++) {
-        let stIndex = line.search(redKeywords[i]);
+        /*let stIndex = line.search(redKeywords[i]);
         let endIndex = stIndex + redKeywords[i].length - 1;
         if (stIndex != -1  && isKeyword(line, stIndex, endIndex)) {
-            line = line.substring(0, stIndex) + "<span style='color: rgb(207, 34, 46);'>" + line.substring(stIndex, stIndex + redKeywords[i].length) + "</span>" + line.substring(stIndex + redKeywords[i].length, line.length);   
-        }
+            line = line.substring(0, stIndex) + "<span style='color: rgb(207, 34, 46);'>" + line.substring(stIndex, stIndex + redKeywords[i].length) + "</span>" + line.substring(stIndex + redKeywords[i].length, line.length);
+        }*/
+		var index= 0;
+		for(let j=0; j< words.length; j++){
+      words[j]=words[j].replace(/[^a-zA-Z ]/g, "")
+
+			if( words[j]== redKeywords[i]){
+      console.log("1"+line);
+      console.log(words[j]);
+			line2 = line.substring(0, index) + "<span style='color: rgb(207, 34, 46);'>" + line.substring(index, index + redKeywords[i].length) + "</span>";
+      line2= line2+ line.substring(index + redKeywords[i].length, line.length);
+      index+=line2.length;
+      line=line2;
+      console.log("2"+line);
+      }
+			index+=words[j].length;
+			index++;
+		}
+
     }
 
     for(let i = 0; i < blueKeywords.length; i++) {
         let stIndex = line.search(blueKeywords[i]);
         let endIndex = stIndex + blueKeywords[i].length - 1;
         if (stIndex != -1 && isKeyword(line, stIndex, endIndex)) {
-            line = line.substring(0, stIndex) + "<span style='color: rgb(5, 80, 174);'>" + line.substring(stIndex, stIndex + blueKeywords[i].length) + "</span>" + line.substring(stIndex + blueKeywords[i].length, line.length);   
+            line = line.substring(0, stIndex) + "<span style='color: rgb(5, 80, 174);'>" + line.substring(stIndex, stIndex + blueKeywords[i].length) + "</span>" + line.substring(stIndex + blueKeywords[i].length, line.length);
         }
     }
 
@@ -86,7 +104,7 @@ function styleKeywords(line){
     if (found != -1) {
         comment = line.substring(found, line.length);
         comment = comment.replace(/<\/?[^>]+(>|$)/g, "");
-        line = line.substring(0, found) + "<span style='color: rgb(110, 119, 129);'>" + comment + "</span>";  
+        line = line.substring(0, found) + "<span style='color: rgb(110, 119, 129);'>" + comment + "</span>";
     }
 
     return line;
